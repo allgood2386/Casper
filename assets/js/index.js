5abdd8739569a4976ac334d325fcabd427a86691
Richard Allen <rallen@taoti.com>
2014-01-13 10:15:39 -0500

    copying changes over from personal repo... never forget to check for a fork

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,4 +12,15 @@
 
     });
 
+    /* Not-Ghost */
+    $(function(){
+        if($(".adsbygoogle").css('display')=="none") {
+            $('.adsbygoogle').replaceWith('<div class="adblocker">Hey, I noticed you have an ad blocker.' +
+                ' Well I\'m a developer paying off student loans still and every bit helps. I\'ve gone and ' +
+                'made this small ad as un-intrusive as possible so if you could consider white listing this site ' +
+                'it would be much appreciated!</div>');
+            $('.adsbygoogle').css('display','block!important' );
+        }
+    });
+
 }(jQuery));
